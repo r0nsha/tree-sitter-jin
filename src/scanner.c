4c1a7d2fd4f19944e613af3e9aa178cdec0d30e6
r0nsha <r0nsh4v1t@gmail.com>
2023-12-12 14:57:59 +0200

    wip

--- src/scanner.c
+++ src/scanner.c
@@ -2,18 +2,18 @@
 
 enum TokenType { QUOTED_CONTENT };
 
-void *tree_sitter_gleam_external_scanner_create() { return NULL; }
-void tree_sitter_gleam_external_scanner_destroy(void *payload) {}
-unsigned tree_sitter_gleam_external_scanner_serialize(void *payload,
-                                                      char *buffer) {
+void *tree_sitter_jin_external_scanner_create() { return NULL; }
+void tree_sitter_jin_external_scanner_destroy(void *payload) {}
+unsigned tree_sitter_jin_external_scanner_serialize(void *payload,
+                                                    char *buffer) {
   return 0;
 }
-void tree_sitter_gleam_external_scanner_deserialize(void *payload,
-                                                    const char *buffer,
-                                                    unsigned length) {}
+void tree_sitter_jin_external_scanner_deserialize(void *payload,
+                                                  const char *buffer,
+                                                  unsigned length) {}
 
-bool tree_sitter_gleam_external_scanner_scan(void *payload, TSLexer *lexer,
-                                             const bool *valid_symbols) {
+bool tree_sitter_jin_external_scanner_scan(void *payload, TSLexer *lexer,
+                                           const bool *valid_symbols) {
   if (valid_symbols[QUOTED_CONTENT]) {
     bool has_content = false;
 
